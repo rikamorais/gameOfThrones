67716a6e1b4b57cf83fe6550ba25f5ab6747a4bf
Érika Morais Xavier <erikausjt@yahoo.com.br>
2021-06-19 19:34:01 -0300

    Atualização do order by

--- pergunta3.sql
+++ pergunta3.sql
@@ -1,3 +1,3 @@
 -- Pergunta 3
 -- Quais foram os episódios melhor avaliados?
-select Season, Episode, Title, Rating, Release_date, Writer_1, Director from data order by Rating desc limit 4;
+select * from (select Season, Episode, Title, Rating, Release_date, Writer_1, Director from data order by Rating desc limit 4) as rating order by Season;
