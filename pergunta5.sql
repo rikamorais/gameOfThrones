aa693cdea75fe71067b35f754790a12a2ea1a712
Érika Morais Xavier <erikausjt@yahoo.com.br>
2021-06-19 19:55:37 -0300

    Atualização da pergunta

--- pergunta5.sql
+++ pergunta5.sql
@@ -1,3 +1,3 @@
 -- Pergunta 5
--- Quais os personagens que mais apareceram em episódios na série?
+-- Quais os personagens que mais apareceram na série?
 Select * from characters_v4 order by Episodes_appeared desc limit 4;
