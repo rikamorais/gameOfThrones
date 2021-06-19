571d0ff6ac63e9804d43b9f3e0e179806a1ea2bc
Érika Morais Xavier <erikausjt@yahoo.com.br>
2021-06-19 19:16:00 -0300

    Tabela diretor adicionada

--- pergunta2.sql
+++ pergunta2.sql
@@ -1,4 +1,4 @@
 -- Pergunta 2
 -- Quando foi lançado o último episódio?
-select season, episode, title, release_date from
+select season, episode, title, release_date, Writer_1, Director from
 	(select * from data order by season desc limit 6) as last_episode order by episode desc limit 1;
