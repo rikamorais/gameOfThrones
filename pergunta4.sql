cacf9bdb023e016a3c98ea85965ce393f545b970
Érika Morais Xavier <erikausjt@yahoo.com.br>
2021-06-19 19:48:08 -0300

    Adicionado outro order by

--- pergunta4.sql
+++ pergunta4.sql
@@ -1,3 +1,3 @@
 -- Pergunta 4
 -- Quais os episódios que os fãs menos gostaram?
-select Season, Episode, Title, Rating, Release_date, Writer_1, Director from data order by Rating asc limit 3;
+select * from (select Season, Episode, Title, Rating, Release_date, Writer_1, Director from data order by Rating asc limit 3) as Rating order by Season asc;
